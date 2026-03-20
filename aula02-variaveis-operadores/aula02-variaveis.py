7a9f2227cc80ade7eeda5e8b5f0d18951e7d5fcd
Gustavo-FS12 <gustavo.f.souza0@gmail.com>
2026-03-19 22:28:49 -0300

    aula 2 variaveis e operadores

---
+++ aula02-variaveis-operadores/aula02-variaveis.py
@@ -0,0 +1,29 @@
+print(7+4)
+print("7+4")
+print("7"+"4") #concatenação de strings
+
+# Comentários de 1 linha
+
+'''
+Comentários de múltiplas linhas
+
+'''
+
+#Variáveis
+nome = "Alexandre"
+print(nome)
+idade = 26 #int
+peso = 70.2 #float
+
+print(nome, idade, peso)
+print(f"oiii, {nome}!!!")
+
+
+# INPUT - simulação de forms no CMD
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(nova_idade)
